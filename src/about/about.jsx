905eb840e8c3d4370b981b4030b1d0f1f6cde2c1
hgspig <42699788+hgspig@users.noreply.github.com>
2023-04-17 11:30:54 -0600

    committing initial file creation

--- src/about/about.jsx
+++ src/about/about.jsx
@@ -2,8 +2,47 @@ import React from 'react';
 
 export function About() {
   return (
-    <main className='container-fluid bg-secondary text-center'>
-      <div>about displayed here</div>
+    <main className='container-fluid removescrolling-sm removescrolling'>
+      <div className="container py-5 h-100">
+        <div className="row d-flex justify-content-center align-items-center h-100">
+          <div className="row col-xl-10">
+            <div
+              className="shadow-lg card second-color"
+            >
+              <div className="row g-0">
+                <div className="d-flex align-items-center">
+                  <div className="card-body p-4 p-lg-5 text-black">
+                    <div className="d-flex align-items-center mb-3 pb-1">
+                      <span className="h1 fw-bold mb-0">Our mission</span>
+                    </div>
+
+                    <h5 className="fw-normal mb-3 pb-3">
+                      Our mission is to help you accomplish all you are setting
+                      out to do. We want to help you be more productive and get
+                      more done in less time. We want to help you be more
+                      organized and have a better life.
+                    </h5>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="row col-xl-10">
+            <div
+              className="shadow-lg card second-color"
+            >
+              <div className="row g-0">
+                <div className="d-flex align-items-center">
+                  <div className="card-body p-4 p-lg-3 text-black">
+                    <div id="quote" className="quote-box"></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+        </div>
+      </div>
     </main>
   );
 }
--- src/stats/stats.jsx
+++ src/stats/stats.jsx
@@ -1,9 +1,141 @@
 import React from 'react';
 
+// go back to minute 32 (I stopped after minute 35)
+
+// const [stats, setStats] = React.useState([])
+
+// React.useEffect(() => {
+//   fetch('/api/stats' + currentUser)
+//   .then((response) => response.json())
+//   .then((stats) => {
+//     setStats(stats);
+//     localStorage.setItem('scores', JSON>stringify(stats));
+//   })
+//   .catch(() => {
+//     const statsText = localStorage.getItem('stats');
+//     if (statsText) {
+//       setStats(JSON.parse(statsText));
+//     }
+//   });
+//   }, []);
+
+// async function statsLoad() {
+//   let schooltaskscompleted = 0;
+//   let schooltaskstotal = 0;
+//   let worktaskscompleted = 0;
+//   let worktaskstotal = 0;
+//   let othertaskscompleted = 0;
+//   let othertaskstotal = 0;
+//   let currentUser = localStorage.getItem("userName");
+//   const response = await fetch("/api/task/" + currentUser);
+//   const tasks = await response.json();
+//   for (i in tasks) {
+//     let task = tasks[i];
+//     if (task.category == "school") {
+//       schooltaskstotal += 1;
+//       if (task.completed == true) {
+//         schooltaskscompleted += 1;
+//       }
+//     }
+//     if (task.category == "work") {
+//       worktaskstotal += 1;
+//       if (task.completed == true) {
+//         worktaskscompleted += 1;
+//       }
+//     }
+//     if (task.category == "other") {
+//       othertaskstotal += 1;
+//       if (task.completed == true) {
+//         othertaskscompleted += 1;
+//       }
+//     }
+//   }
+//   main();
+// }
+
+
+
 export function Stats() {
   return (
-    <main className='container-fluid bg-secondary text-center'>
-      <div>Stats displayed here</div>
+    <main classNameName='container-fluid text-center'>
+      <div className="removescrolling-sm removescrolling">
+      <div className="row">
+        <section className="col-sm">
+          <div className="container py-4 h-100">
+            <div className="shadow card">
+              <div className="second-color card-header">School</div>
+              <div className="card-body">
+                <blockquote className="blockquote mb-0">
+                  <p id="schooltaskmessage">
+                    Your currently <span id="schoolpercent"></span> through your
+                    tasks in school
+                  </p>
+                  <div className="progress">
+                    <div
+                      className="third-color progress-bar progress-bar-striped progress-bar-animated"
+                      role="progressbar"
+                      id="schoolpercentagevalueHTML"
+                      aria-label="Animated striped example"
+                    ></div>
+                  </div>
+                </blockquote>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+      <div className="row">
+        <section className="col-sm">
+          <div className="container py-4 h-100">
+            <div className="shadow card">
+              <div className="second-color card-header">Work</div>
+              <div className="card-body">
+                <blockquote className="blockquote mb-0">
+                  <p id="worktaskmessage">
+                    Your currently <span id="workpercent"></span> through your
+                    tasks in work
+                  </p>
+                  <div className="progress">
+                    <div
+                      className="third-color progress-bar progress-bar-striped progress-bar-animated"
+                      role="progressbar"
+                      id="workpercentagevalueHTML"
+                      aria-label="Animated striped example"
+                    ></div>
+                  </div>
+                </blockquote>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+
+      <div className="row">
+        <section className="col-sm">
+          <div className="container py-4 h-100">
+            <div className="shadow card">
+              <div className="second-color card-header">Other</div>
+              <div className="card-body">
+                <blockquote className="blockquote mb-0">
+                  <p id="othertaskmessage">
+                    Your currently <span id="otherpercent"></span> through your
+                    other tasks
+                  </p>
+                  <div className="progress">
+                    <div
+                      className="third-color progress-bar progress-bar-striped progress-bar-animated"
+                      role="progressbar"
+                      id="otherpercentagevalueHTML"
+                      aria-label="Animated striped example"
+                    ></div>
+                  </div>
+                </blockquote>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+    </div>
     </main>
   );
 }
